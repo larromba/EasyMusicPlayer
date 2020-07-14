bff4b92733495b459a886f9f0a533061edc8a09b
Lee Arromba <larromba@gmail.com>
2020-07-14 17:25:01 +0100

    + sanity tests

--- EasyMusicPlayerTests/SanityTests.swift
+++ EasyMusicPlayerTests/SanityTests.swift
@@ -3,17 +3,20 @@ import MediaPlayer
 import XCTest
 
 final class ExpTests: XCTestCase {
+    private var player: AVAudioPlayer!
+
     override func setUp() {
         super.setUp()
     }
 
     override func tearDown() {
+        player = nil
         super.tearDown()
     }
 
-    func test_experiement() {
+    func test_audio_whenURLLoaded_expectPlays() {
         do {
-            let player = try AVAudioPlayer(contentsOf: DummyAsset.endSilence.url)
+            player = try AVAudioPlayer(contentsOf: DummyAsset.endSilence.url)
             XCTAssertTrue(player.prepareToPlay())
             XCTAssertTrue(player.play())
         } catch {
