b2ba59756db1d3391784558d1d8d5f125e276632
Lee <676915+larromba@users.noreply.github.com>
2025-06-12 13:24:07 +0200

    PlayerViewModelTests Swift 6.0

--- Code/EasyMusicPlayerTests/PlayerViewModelTests.swift
+++ Code/EasyMusicPlayerTests/PlayerViewModelTests.swift
@@ -1,79 +1,85 @@
 @testable import EasyMusicPlayer
-import XCTest
+import Testing
 
 @MainActor
-final class PlayerViewModelTests: XCTestCase {
-    private var musicPlayer: MusicPlayableMock!
-    private var urlSharer: URLSharableMock!
-    private var sut: PlayerViewModel!
+@Suite(.serialized)
+struct PlayerViewModelTests {
+    private let musicPlayer: MusicPlayableMock
+    private let urlSharer: URLSharableMock
+    private let sut: PlayerViewModel
 
-    override func setUpWithError() throws {
-        musicPlayer = MusicPlayableMock()
-        urlSharer = URLSharableMock()
+    init() {
+        let musicPlayer = MusicPlayableMock()
+        self.musicPlayer = musicPlayer
+
+        let urlSharer = URLSharableMock()
+        self.urlSharer = urlSharer
+
         sut = PlayerViewModel(musicPlayer: musicPlayer, urlSharer: urlSharer)
-    }
-
-    override func tearDownWithError() throws {
-        musicPlayer = nil
-        urlSharer = nil
-        sut = nil
     }
 
     // MARK: - authorize
 
-    func test_authorize_whenInvoked_expectIsAuthorised() {
+    @Test
+    func authorize_whenInvoked_expectIsAuthorised() {
         sut.authorize()
 
-        XCTAssertEqual(musicPlayer.authorizeCallCount, 1)
+        #expect(musicPlayer.authorizeCallCount == 1)
     }
 
     // MARK: - about
 
-    func test_about_whenInvoked_expectOpensAbout() {
+    @Test
+    func about_whenInvoked_expectOpensAbout() {
         sut.openAbout()
 
-        XCTAssertEqual(urlSharer.openCallCount, 1)
+        #expect(urlSharer.openCallCount == 1)
     }
 
     // MARK: - state
 
-    func test_state_whenErrorAuthReceived_expectAlert() {
+    @Test
+    func state_whenErrorAuthReceived_expectAlert() {
         musicPlayer.stateSubject.send(.error(.auth))
 
-        XCTAssertEqual(sut.alert.title, L10n.authorizationErrorTitle)
-        XCTAssertEqual(sut.alert.text, L10n.authorizationErrorMessage)
-        XCTAssertEqual(sut.alert.buttonTitle, L10n.authorizationErrorButton)
+        #expect(sut.alert.title == L10n.authorizationErrorTitle)
+        #expect(sut.alert.text == L10n.authorizationErrorMessage)
+        #expect(sut.alert.buttonTitle == L10n.authorizationErrorButton)
     }
 
-    func test_state_whenErrorFinishedReceived_expectAlert() {
+    @Test
+    func state_whenErrorFinishedReceived_expectAlert() {
         musicPlayer.stateSubject.send(.error(.finished))
 
-        XCTAssertEqual(sut.alert.title, L10n.finishedAlertTitle)
-        XCTAssertEqual(sut.alert.text, L10n.finishedAlertMsg)
-        XCTAssertEqual(sut.alert.buttonTitle, L10n.finishedAlertButton)
+        #expect(sut.alert.title == L10n.finishedAlertTitle)
+        #expect(sut.alert.text == L10n.finishedAlertMsg)
+        #expect(sut.alert.buttonTitle == L10n.finishedAlertButton)
     }
 
-    func test_state_whenNoMusicReceived_expectAlert() {
+    @Test
+    func state_whenNoMusicReceived_expectAlert() {
         musicPlayer.stateSubject.send(.error(.noMusic))
 
-        XCTAssertEqual(sut.alert.title, L10n.noMusicErrorTitle)
-        XCTAssertEqual(sut.alert.text, L10n.noMusicErrorMsg)
-        XCTAssertEqual(sut.alert.buttonTitle, L10n.noMusicErrorButton)
+        #expect(sut.alert.title == L10n.noMusicErrorTitle)
+        #expect(sut.alert.text == L10n.noMusicErrorMsg)
+        #expect(sut.alert.buttonTitle == L10n.noMusicErrorButton)
     }
 
-    func test_state_whenPlayReceived_expectAlert() {
+    @Test
+    func state_whenPlayReceived_expectAlert() {
         musicPlayer.stateSubject.send(.error(.play))
 
-        XCTAssertEqual(sut.alert.title, L10n.playErrorTitle)
-        XCTAssertEqual(sut.alert.text, L10n.playErrorMessage)
-        XCTAssertEqual(sut.alert.buttonTitle, L10n.playErrorButton)
+        #expect(sut.alert.title == L10n.playErrorTitle)
+        #expect(sut.alert.text == L10n.playErrorMessage)
+        #expect(sut.alert.buttonTitle == L10n.playErrorButton)
     }
 
-    func test_state_whenVolumeReceived_expectAlert() {
+    @Test
+    func state_whenVolumeReceived_expectAlert() {
         musicPlayer.stateSubject.send(.error(.volume))
 
-        XCTAssertEqual(sut.alert.title, L10n.noVolumeErrorTitle)
-        XCTAssertEqual(sut.alert.text, L10n.noVolumeErrorMsg)
-        XCTAssertEqual(sut.alert.buttonTitle, L10n.noVolumeErrorButton)
+        #expect(sut.alert.title == L10n.noVolumeErrorTitle)
+        #expect(sut.alert.text == L10n.noVolumeErrorMsg)
+        #expect(sut.alert.buttonTitle == L10n.noVolumeErrorButton)
     }
 }
