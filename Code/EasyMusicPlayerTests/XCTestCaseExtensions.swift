49501b98227687c52c02b4afd5c0aa0fa5b9c5ff
Lee <676915+larromba@users.noreply.github.com>
2025-06-12 13:25:01 +0200

    + testing utilties

--- Code/EasyMusicPlayerTests/XCTestCaseExtensions.swift
+++ Code/EasyMusicPlayerTests/XCTestCaseExtensions.swift
@@ -1,6 +1,7 @@
 import Foundation
 import XCTest
 
+@MainActor
 public extension XCTestCase {
     // see https://www.vadimbulavin.com/swift-asynchronous-unit-testing-with-busy-assertion-pattern/
     func waitSync(for duration: TimeInterval = 0.5) {
@@ -8,12 +9,15 @@ public extension XCTestCase {
     }
 
     // see https://stackoverflow.com/questions/31182637/delay-wait-in-a-test-case-of-xcode-ui-testing
-    func waitAsync(for duration: TimeInterval = 0.5, delay: TimeInterval = 0.0, queue: DispatchQueue = .main,
-                   completion: @escaping (@escaping () -> Void) -> Void) {
+    func waitAsync(
+        for duration: TimeInterval = 0.5, delay: TimeInterval = 0.0,
+        queue: DispatchQueue = .main,
+        completion: @escaping @Sendable (@escaping @Sendable () -> Void) -> Void
+    ) {
         let expectation = self.expectation(description: "wait asynchronously for callback")
         queue.asyncAfter(deadline: .now() + delay) {
             completion {
-                DispatchQueue.main.async { expectation.fulfill() }
+                queue.async { expectation.fulfill() }
             }
         }
         waitForExpectations(timeout: delay + duration)
